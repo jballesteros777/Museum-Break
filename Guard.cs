c3cd7178b4f1d290ed654d0aaac60ab2c9e2f967
jballesteros777 <46576580+jballesteros777@users.noreply.github.com>
2021-02-25 11:07:49 -1000

    Add files via upload

---
+++ Guard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour {
+    
+    void Update() {
+        //transform.Rotate(0f, 0f, 0f);
+    }
+}
+  
