92f40fec31024a4b7ca4d033f523053ced6b8f24
jballesteros777 <46576580+jballesteros777@users.noreply.github.com>
2021-02-25 10:21:21 -1000

    GameManager

---
+++ GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    //public GameObject playerPrefab;
+    private bool gameOver = false;
+
+    public Player player;
+    public Guard guard;
+
+    public Animator animator;
+    public Rigidbody2D isplayer;
+    
+    void Update() {
+        if(Input.GetKeyDown("space")) 
+            isplayer.MovePosition(isplayer.position + Vector2.right * 10f * Time.deltaTime);
+        
+    }
+    public void EndGame() {
+        if(gameOver){
+            return;
+        }
+
+        animator.SetTrigger("EndGame");
+        gameOver = true;
+        player.enabled = false;
+    }
+
+    //public void RestartLevel(){
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //}
+}
